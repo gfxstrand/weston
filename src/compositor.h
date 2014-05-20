e9714f83056f654c32630d1441650c8b888dbf81
Jason Ekstrand <jason@jlekstrand.net>
2014-05-20 17:57:08 -0500

    weston_surface: Add surface-to-buffer and buffer-to-surface matrices

--- src/compositor.h
+++ src/compositor.h
@@ -859,6 +859,12 @@ struct weston_surface {
 	struct weston_surface_state pending;
 	struct wl_listener pending_buffer_destroy_listener;
 
+	/* Matrices representating of the full transformation between
+	 * buffer and surface coordinates.  These matrices are updated
+	 * using the weston_surface_build_buffer_matrix function. */
+	struct weston_matrix buffer_to_surface_matrix;
+	struct weston_matrix surface_to_buffer_matrix;
+
 	/*
 	 * If non-NULL, this function will be called on
 	 * wl_surface::commit after a new buffer has been set up for
